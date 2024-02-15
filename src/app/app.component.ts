39796423a84c5749fdd1fb25e47422c2a2c4b53d
Vaishnavi Kesherwani <vaishnavikesherwani786@gmail.com>
2024-02-15 14:05:59 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+
+})
+export class AppComponent {
+  title = 'Angular-Directives';
+  sdata: any;
+  rdata(val: any) {
+    this.sdata = val
+  }
+}
